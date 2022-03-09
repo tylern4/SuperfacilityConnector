b7410b6a0b1d9b494da82ac625b96f541f76eec0
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-03-09 14:49:02 -0800

    Quick fix for older version

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@
 import os
 from setuptools import setup
 from pathlib import Path
+import sys
 
 _dir = Path(__file__).resolve().parent
 
 with open(f"{_dir}/README.md") as f:
     long_desc = f.read()
-
-
+ 
+if sys.version_info >= (3,7,0):
+    install_requires=['authlib', 'requests', 'click', 'tabulate', 'pandas', 'numpy']
+else:
+    install_requires=['authlib', 'requests']
+    
 setup(
     name="SuperfacilityConnector",
     description="Connector API NERSC Superfacility",
@@ -22,9 +27,7 @@ setup(
     package_dir={'': 'python'},
     version='0.0.2',
     scripts=['python/SuperfacilityAPI/bin/sfapi'],
-    install_requires=[
-        'authlib', 'requests', 'click', 'tabulate', 'pandas', 'numpy'
-    ],
+    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
