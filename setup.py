267ff4612b7b46750982cca0b4b99a611ca2fcaa
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-03-11 10:37:02 -0800

    Update version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     author_email="tylern@lbl.gov",
     packages=['SuperfacilityAPI'],
     package_dir={'': 'python'},
-    version='0.0.2',
+    version='0.0.3',
     scripts=['python/SuperfacilityAPI/bin/sfapi'],
     install_requires=install_requires,
     classifiers=[
