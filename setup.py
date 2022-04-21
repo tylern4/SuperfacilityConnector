2e417c816e202a9e9667b2e82704ceda69866ba8
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-04-21 13:13:24 -0700

    version bumps

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     author_email="tylern@lbl.gov",
     packages=['SuperfacilityAPI'],
     package_dir={'': 'python'},
-    version='0.1.1',
+    version='0.1.2',
     scripts=['python/SuperfacilityAPI/bin/sfapi'],
     install_requires=install_requires,
     classifiers=[
